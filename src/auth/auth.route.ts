357bde06d155800fa5ccc35f9424ada0ec1c83ca
lilian <deceuninck.lilian1@gmail.com>
2026-02-13 13:37:49 +0100

    fix(auth): fix route

--- src/auth/auth.route.ts
+++ src/auth/auth.route.ts
@@ -5,8 +5,8 @@ export const authRouter = Router()
 
 // POST /api/auth/sign-up
 // Accessible via POST /api/auth/sign-up
-authRouter.post('/', authController.signUp)
+authRouter.post('/sign-up', authController.signUp)
 
 // POST api/auth/sign-in
 // Accessible via POST api/auth/sign-in
-authRouter.post('/', authController.signIn)
+authRouter.post('/sign-in', authController.signIn)
