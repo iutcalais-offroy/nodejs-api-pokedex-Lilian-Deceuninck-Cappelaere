d52231f3323ab68f755403245ade2c566b971b3e
lilian <deceuninck.lilian1@gmail.com>
2026-02-28 14:11:32 +0100

    feat(Système de Jeu): add drawCards and simplifies display

--- src/socket/socket.match.ts
+++ src/socket/socket.match.ts
@@ -3,6 +3,7 @@ import { decksService } from '../decks/decks.service'
 import { Card } from '../generated/prisma/client'
 
 interface Player {
+  socketId: string
   playerId: number
   email: string
   deck: Card[]
@@ -22,10 +23,10 @@ interface Room {
 }
 
 interface Game {
-  gameId: number
+  gameId: string
   player1: Player
   player2: Player
-  turn: number
+  turn: string
 }
 
 // Fonction pour mélanger le deck
@@ -58,6 +59,7 @@ export const matchMaking = (io: Server) => {
         const cards = shuffle(deck.cards.map((carte: Card) => carte.card))
 
         const player1: Player = {
+          socketId: socket.id,
           playerId: socket.user.userId,
           email: socket.user.email,
           deck: cards.slice(5),
@@ -132,6 +134,7 @@ export const matchMaking = (io: Server) => {
           const cards = shuffle(deck.cards.map((carte: Card) => carte.card))
 
           const player2: Player = {
+            socketId: socket.id,
             playerId: socket.user.userId,
             email: socket.user.email,
             deck: cards.slice(5),
@@ -153,10 +156,10 @@ export const matchMaking = (io: Server) => {
             socket.emit('error', 'Joueur 1 introuvable')
           } else {
             const newGame: Game = {
-              gameId: Number(data.roomId),
+              gameId: data.roomId,
               player1: player1,
               player2: player2,
-              turn: 1,
+              turn: player1.email,
             }
             games.push(newGame)
 
@@ -173,18 +176,10 @@ export const matchMaking = (io: Server) => {
 
             io.to(data.roomId).emit(
               'gameStarted',
-              `La partie entre ${player1.email} et ${player2.email} commence`,
+              `La partie entre ${player1.email} et ${player2.email} commence au tour de ${newGame.turn}`,
             )
-            socket
-              .to(data.roomId)
-              .emit(
-                'hand',
-                `Votre main (nom du pokemon, hp, attaque, type) : ${player1.hand.map((carte) => [carte.name, carte.hp, carte.attack, carte.type]).join('; ')}`,
-              )
-            socket.emit(
-              'hand',
-              `Votre main (nom du pokemon, hp, attaque, type) : ${player2.hand.map((carte) => [carte.name, carte.hp, carte.attack, carte.type]).join('; ')}`,
-            )
+
+            gameState(newGame, 'gameStarted')
           }
         }
       } catch (error: unknown) {
@@ -200,5 +195,106 @@ export const matchMaking = (io: Server) => {
         }
       }
     })
+
+    socket.on('drawCards', async (data: { roomId: string }) => {
+      const game = games.find((game: Game) => game.gameId === data.roomId)
+      if (!game) {
+        return socket.emit('error', 'Partie introuvable')
+      }
+
+      let currentPlayer: Player
+      let playerEmail: string
+
+      if (socket.user.userId === game.player1.playerId) {
+        currentPlayer = game.player1
+        playerEmail = game.player1.email
+      } else if (socket.user.userId === game.player2.playerId) {
+        currentPlayer = game.player2
+        playerEmail = game.player2.email
+      } else {
+        return socket.emit('error', "Vous n'êtes pas joueur à cette partie")
+      }
+
+      if (game.turn !== playerEmail) {
+        return socket.emit('error', "Ce n'est pas votre tour")
+      }
+
+      if (currentPlayer.hand.length === 5) {
+        return socket.emit('error', 'Vous avez déjà 5 cartes')
+      }
+
+      while (currentPlayer.hand.length < 5 && currentPlayer.deck.length > 0) {
+        const card = currentPlayer.deck.shift()
+
+        if (card) {
+          currentPlayer.hand.push(card)
+        } else {
+          return socket.emit('error')
+        }
+      }
+
+      return gameState(game)
+    })
+
+    function cardsLabel(tabCards: Card[] | Card | null) {
+      // field vide
+      if (tabCards === null) {
+        return null
+      }
+
+      // hand
+      if (Array.isArray(tabCards)) {
+        return tabCards.map((carte) => ({
+          nom_du_pokemon: carte.name,
+          hp: carte.hp,
+          attaque: carte.attack,
+          type: carte.type,
+        }))
+      }
+
+      // field
+      return {
+        nom_du_pokemon: tabCards.name,
+        hp: tabCards.hp,
+        attaque: tabCards.attack,
+        type: tabCards.type,
+      }
+    }
+
+    function gameState(game: Game, message?: string) {
+      const messageEmit: string = message || 'gameStateUpdated'
+
+      // Joueur 1
+      io.to(String(game.player1.socketId)).emit(messageEmit, {
+        vous: {
+          main: cardsLabel(game.player1.hand),
+          terrain: cardsLabel(game.player1.field),
+          hp_pokemon_terrain: game.player1.HP,
+          score: game.player1.score,
+        },
+        'votre adversaire': {
+          nombre_carte_en_main: game.player2.hand.length,
+          terrain: cardsLabel(game.player2.field),
+          score: game.player2.score,
+        },
+        tour: `tour du joueur ${game.turn}`,
+      })
+
+      // Joueur 2
+      io.to(String(game.player2.socketId)).emit(messageEmit, {
+        vous: {
+          main: cardsLabel(game.player2.hand),
+          terrain: cardsLabel(game.player2.field),
+          hp_pokemon_terrain: game.player2.HP,
+          score: game.player2.score,
+        },
+        'votre adversaire': {
+          nombre_carte_en_main: game.player1.hand.length,
+          terrain: cardsLabel(game.player1.field),
+          score: game.player1.score,
+        },
+        tour: `tour du joueur ${game.turn}`,
+      })
+    }
   })
 }
